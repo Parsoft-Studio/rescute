bbf248c3ca71ab472ab09e96611df36d293915f5
ehossein <ehsan.hosseinkhani@axelhealth.com>
2022-04-20 14:06:12 +0300

    Added UI files

---
+++ rescute.UI/scripts/rescute-report.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    const gallery = new Viewer(document.getElementById('events'), {
+        toolbar: false,
+        navbar: false,
+        button: false,
+        title: false,
+        toggleOnDblclick: false,
+        slideOnTouch: false
+    });
+    $('[data-toggle="tooltip"]').tooltip();
+});
