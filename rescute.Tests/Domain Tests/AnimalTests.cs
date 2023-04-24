87f58687aafda6f00a3402a8893e3a3a0c959481
pbisadi <p.bisadi@gmail.com>
2023-04-24 02:00:03 -0400

    Refactor domain unit test using ChatGPT

--- rescute.Tests/Domain Tests/AnimalTests.cs
+++ rescute.Tests/Domain Tests/AnimalTests.cs
@@ -25,5 +25,142 @@ namespace rescute.Tests.DomainTests
             animal1.Should().NotBe(animal2);
         }
 
+        [Fact]
+        public void AnimalTypeIsSetCorrectlyWhenCreated()
+        {
+            // Arrange
+            var samaritan = TestUtility.RandomTestSamaritan();
+            var registrationDate = DateTime.Now;
+            var introducedBy = samaritan.Id;
+            var description = "Test description";
+            var animalType = AnimalType.Dog();
+
+            // Act
+            var animal = new Animal(registrationDate, introducedBy, description, animalType);
+
+            // Assert
+            animal.Type.Should().Be(animalType);
+        }
+
+        [Fact]
+        public void AcceptableAttachmentTypes_ReturnsExpectedTypes()
+        {
+            var animal = TestUtility.RandomTestAnimal(TestUtility.RandomTestSamaritan().Id);
+
+            var expectedTypes = new List<AttachmentType> { AttachmentType.Image() };
+            var actualTypes = animal.AcceptableAttachmentTypes.ToList();
+
+            actualTypes.Should().BeEquivalentTo(expectedTypes);
+        }
+
+        [Fact]
+        public void AddAttachments_ShouldAddAttachmentsToList()
+        {
+            // Arrange
+            var samaritan = TestUtility.RandomTestSamaritan();
+            var animal = TestUtility.RandomTestAnimal(samaritan.Id);
+            var attachment1 = new Attachment("image1.jpg", "jpg", DateTime.Now, "Test image 1");
+            var attachment2 = new Attachment("image2.jpg", "jpg", DateTime.Now, "Test image 2");
+
+            // Act
+            animal.AddAttachments(attachment1, attachment2);
+
+            // Assert
+            animal.Attachments.Should().Contain(attachment1);
+            animal.Attachments.Should().Contain(attachment2);
+
+        }
+
+        [Fact]
+        public void RemoveAttachment_RemovesAttachmentFromList()
+        {
+            // Arrange
+            var samaritan = TestUtility.RandomTestSamaritan();
+            var animal = TestUtility.RandomTestAnimal(samaritan.Id);
+            var attachment = TestUtility.RandomTestAttachment();
+
+            animal.AddAttachments(attachment);
+
+            // Act
+            animal.RemoveAttachment(attachment);
+
+            // Assert
+            animal.Attachments.Should().NotContain(attachment);
+        }
+
+        [Fact]
+        public void ClearAttachments_ClearsAttachmentsList()
+        {
+            var samaritan = TestUtility.RandomTestSamaritan();
+            var animal = TestUtility.RandomTestAnimal(samaritan.Id);
+            var attachment1 = TestUtility.RandomTestAttachment();
+            var attachment2 = TestUtility.RandomTestAttachment();
+            animal.AddAttachments(attachment1, attachment2);
+
+            animal.ClearAttachments();
+
+            animal.Attachments.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void UpdateRegistrationDate_UpdatesRegistrationDatePropertyCorrectly()
+        {
+            // Arrange
+            var samaritan = TestUtility.RandomTestSamaritan();
+            var animal = TestUtility.RandomTestAnimal(samaritan.Id);
+            var newRegDate = DateTime.Now.AddDays(-7);
+
+            // Act
+            animal.UpdateRegistrationDate(newRegDate);
+
+            // Assert
+            animal.RegistrationDate.Should().Be(newRegDate);
+        }
+
+        [Fact]
+        public void UpdateDescription_UpdatesDescriptionPropertyCorrectly()
+        {
+            // Arrange
+            var samaritan = TestUtility.RandomTestSamaritan();
+            var animal = TestUtility.RandomTestAnimal(samaritan.Id);
+            var newDescription = "This is the new description.";
+
+            // Act
+            animal.UpdateDescription(newDescription);
+
+            // Assert
+            animal.Description.Should().Be(newDescription);
+        }
+
+        [Fact]
+        public void UpdateAnimalType_ShouldUpdateTypeProperty()
+        {
+            // Arrange
+            var samaritan = TestUtility.RandomTestSamaritan();
+            var animal = TestUtility.RandomTestAnimal(samaritan.Id);
+            var newType = AnimalType.Dog();
+
+            // Act
+            animal.UpdateAnimalType(newType);
+
+            // Assert
+            animal.Type.Should().Be(newType);
+        }
+
+        [Fact]
+        public void UpdateIntroductionSamaritan_UpdatesIntroducedByPropertyCorrectly()
+        {
+            // Arrange
+            var samaritan = TestUtility.RandomTestSamaritan();
+            var animal = TestUtility.RandomTestAnimal(samaritan.Id);
+            var newSamaritan = TestUtility.RandomTestSamaritan();
+
+            // Act
+            animal.UpdateIntroductionSamaritan(newSamaritan.Id);
+
+            // Assert
+            animal.IntroducedBy.Should().Be(newSamaritan.Id);
+        }
+
     }
 }
--- rescute.Tests/Domain Tests/TimelineEventTests.cs
+++ rescute.Tests/Domain Tests/TimelineEventTests.cs
@@ -1,61 +1,77 @@
 ﻿using FluentAssertions;
+
 using rescute.Domain.Aggregates;
 using rescute.Domain.Aggregates.TimelineItems;
 using rescute.Domain.Exceptions;
 using rescute.Domain.ValueObjects;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xunit;
 
 namespace rescute.Tests.DomainTests
 {
     public class TimelineItemTests
     {
+        private readonly Samaritan _samaritan;
+        private readonly Animal _animal;
+        private readonly Attachment _pdfAttachment;
+        private readonly Attachment _jpgAttachment;
+        private readonly Attachment _mp4Attachment;
+        private readonly Attachment _invalidAttachment;
+
+        public TimelineItemTests()
+        {
+            _samaritan = TestUtility.RandomTestSamaritan();
+            _animal = TestUtility.RandomTestAnimal(_samaritan.Id);
+            _pdfAttachment = new Attachment("filename", "pdf", DateTime.Now, string.Empty);
+            _jpgAttachment = new Attachment("filename", "jpg", DateTime.Now, string.Empty);
+            _mp4Attachment = new Attachment("filename", "mp4", DateTime.Now, string.Empty);
+            _invalidAttachment = new Attachment("filename", "fle", DateTime.Now, string.Empty);
+        }
+
         [Fact]
         public void BillRequestsContributions()
         {
             var billTotal = 5000;
+
+            var attachment = _pdfAttachment;
+            var bill = new Bill(DateTime.Now, _samaritan.Id, _animal.Id, "All the costs.", billTotal, false, false, true, null, attachment);
+
+            var request = bill.RequestContribution();
+            var internalRequest = bill.ContributionRequest;
+
+            request.Should().NotBe(null);
+            request.Should().Be(internalRequest);
+        }
+
+
+        [Fact]
+        public void BillDoesntAcceptExcessContribution()
+        {
+            const string transactionId = "TRANSACTION_ID";
+            const int billTotal = 5000;
+
             var samaritan = TestUtility.RandomTestSamaritan();
+            var contributor = TestUtility.RandomTestSamaritan();
             var attachment = new Attachment("test.pdf", "pdf", DateTime.Now, "Attachment description");
 
             var animal = TestUtility.RandomTestAnimal(samaritan.Id);
 
             var bill = new Bill(DateTime.Now, samaritan.Id, animal.Id, "All the costs.", billTotal, false, false, true, null, attachment);
-
-            var request = bill.RequestContribution();
-            var internalRequest = bill.ContributionRequest;
-
-
-            request.Should().NotBe(null);
-            request.Should().Be(internalRequest);
-        }
-        [Fact]
-        public void BillDoesntAcceptExcessContribution()
-        {
-            var transactionId = "TRANSACTION_ID";
-            var billTotal = 5000;
-            var samaritan = TestUtility.RandomTestSamaritan();
-            var contributor = TestUtility.RandomTestSamaritan();
-            var attachment = new Attachment("test.pdf", "pdf", DateTime.Now, "Attachment description");
-
-            bool includesVetFee = true, includesPrescription = false, includesLabResults = false;
-
-            var animal = TestUtility.RandomTestAnimal(samaritan.Id);
-
-            var bill = new Bill(DateTime.Now, samaritan.Id, animal.Id, "All the costs.", billTotal, includesLabResults, includesPrescription, includesVetFee, null, attachment);
             bill.RequestContribution();
 
             var contrib = new Contribution(DateTime.Now, billTotal + 1, contributor.Id, transactionId, "My contribution.");
 
-            Action action = () =>
-            {
-                bill.Contribute(contrib, includesLabResults, includesPrescription, includesVetFee);
-            };
+            Action action = () => bill.Contribute(contrib, false, false, true);
+
             action.Should().Throw<ContributionExceedsRequirement>();
         }
+
         [Fact]
         public void BillIsConsistentWithAttachedMedicalDocuments()
         {
@@ -63,32 +79,26 @@ namespace rescute.Tests.DomainTests
             var contribution_comment = "Here, have this contribution.";
             var contributionAmount = 100000;
 
-            var samaritan = TestUtility.RandomTestSamaritan();
-            var animal = TestUtility.RandomTestAnimal(samaritan.Id);
             var contributor = TestUtility.RandomTestSamaritan();
             var contribution = new Contribution(DateTime.Now, contributionAmount, contributor.Id, "TRANSACTION_ID", contribution_comment);
 
-            var billAttachment = new Attachment("filename", "pdf", DateTime.Now, string.Empty);
-            var labAttachment = new Attachment("filename", "jpg", DateTime.Now, string.Empty);
-            var prescriptionAttachment = new Attachment("filename", "jpg", DateTime.Now, string.Empty);
-
             var medicalDocuments = new List<MedicalDocument>();
-            medicalDocuments.Add(new MedicalDocument(DateTime.Now, samaritan.Id, animal.Id, "Document description.", MedicalDocumentType.Prescription(), prescriptionAttachment));
-            medicalDocuments.Add(new MedicalDocument(DateTime.Now, samaritan.Id, animal.Id, "Document description.", MedicalDocumentType.LabResults(), labAttachment));
+            medicalDocuments.Add(new MedicalDocument(DateTime.Now, _samaritan.Id, _animal.Id, "Document description.", MedicalDocumentType.Prescription(), _jpgAttachment));
+            medicalDocuments.Add(new MedicalDocument(DateTime.Now, _samaritan.Id, _animal.Id, "Document description.", MedicalDocumentType.LabResults(), _jpgAttachment));
 
             var bill = new Bill(DateTime.Now,
-                samaritan.Id,
-                animal.Id,
+                _samaritan.Id,
+                _animal.Id,
                 "I can't pay this on my own!",
                 billAmount,
                 includesLabResults: true,
                 includesPrescription: true,
                 includesVetFee: true,
                 medicalDocuments,
-                billAttachment);
-
+                _pdfAttachment);
 
             bill.RequestContribution();
+
             Action action = () =>
             {
                 bill.Contribute(contribution,
@@ -97,14 +107,16 @@ namespace rescute.Tests.DomainTests
                     includesVetFee: false);
             };
             action.Should().Throw<InconsistentBill>();
+
             action = () =>
-             {
-                 bill.Contribute(contribution,
-                     includesLabResults: true,
-                     includesPrescription: false,
-                     includesVetFee: true);
-             };
+            {
+                bill.Contribute(contribution,
+                    includesLabResults: true,
+                    includesPrescription: false,
+                    includesVetFee: true);
+            };
             action.Should().Throw<InconsistentBill>();
+
             action = () =>
             {
                 bill.Contribute(contribution,
@@ -122,49 +134,29 @@ namespace rescute.Tests.DomainTests
                     includesVetFee: true);
             };
             action.Should().NotThrow<InconsistentBill>();
-
-
         }
+
         [Fact]
-        public void BillAcceptsContribution()
+        public void ContributionIsAddedToBillWhenAccepted()
         {
-            var billAmount = 150000;
-            var contribution_comment = "Here, have this contribution.";
-            var contributionAmount = 100000;
-
-            var samaritan = TestUtility.RandomTestSamaritan();
+            const int billAmount = 150000;
+            const int contributionAmount = 100000;
+            const string contributionComment = "Here, have this contribution.";
             var contributor = TestUtility.RandomTestSamaritan();
-            var contribution = new Contribution(DateTime.Now, contributionAmount, contributor.Id, "TRANSACTION_ID", contribution_comment);
-            bool includesVetFee = true, includesPrescription = false, includesLabResults = false;
+            var contribution = new Contribution(DateTime.Now, contributionAmount, contributor.Id, "TRANSACTION_ID", contributionComment);
 
-            var animal = TestUtility.RandomTestAnimal(samaritan.Id);
-
-            var bill = new Bill(DateTime.Now,
-                samaritan.Id,
-                animal.Id,
-                "I can't pay this on my own!",
-                billAmount,
-                includesLabResults,
-                includesPrescription,
-                includesVetFee,
-                null,
-                new Attachment("filename", "pdf", DateTime.Now, string.Empty)
-                );
-
+            var bill = new Bill(DateTime.Now, _samaritan.Id, _animal.Id, "I can't pay this on my own!", billAmount, true, false, false, null, _pdfAttachment);
             bill.RequestContribution();
-            bill.Contribute(contribution, includesLabResults, includesPrescription, includesVetFee);
+            bill.Contribute(contribution, true, false, false);
 
-
-            var same = bill.ContributionRequest.Contributions.First();
-
-            same.Should().NotBeNull();
             bill.ContributionRequest.Contributions.Count.Should().Be(1);
+            var same = bill.ContributionRequest.Contributions.First();
             same.ContributorId.Should().Be(contributor.Id);
             same.Amount.Should().Be(contributionAmount);
         }
 
         [Fact]
-        public void EventsOnlyAcceptProperDocumentTypes()
+        public void StatusReportOnlyAcceptsProperDocumentTypes()
         {
             var samaritan = TestUtility.RandomTestSamaritan();
             var animal = TestUtility.RandomTestAnimal(samaritan.Id);
@@ -172,7 +164,6 @@ namespace rescute.Tests.DomainTests
             var docImage = new Attachment("filename", "jpg", DateTime.Now, string.Empty);
             var docVid = new Attachment("filename", "mp4", DateTime.Now, string.Empty);
             var docFile = new Attachment("filename", "fle", DateTime.Now, string.Empty);
-            var docPdf = new Attachment("filename", "pdf", DateTime.Now, string.Empty);
             var point = new MapPoint(0, 0);
 
             // StatusReport
@@ -189,9 +180,19 @@ namespace rescute.Tests.DomainTests
             });
 
             statusReportedInvalidAction.Should().Throw<InvalidAttachmentType>();
+        }
 
+        [Fact]
+        public void BillOnlyAcceptsProperDocumentTypes()
+        {
+            var samaritan = TestUtility.RandomTestSamaritan();
+            var animal = TestUtility.RandomTestAnimal(samaritan.Id);
 
-            // Bill
+            var docImage = new Attachment("filename", "jpg", DateTime.Now, string.Empty);
+            var docVid = new Attachment("filename", "mp4", DateTime.Now, string.Empty);
+            var docFile = new Attachment("filename", "fle", DateTime.Now, string.Empty);
+            var docPdf = new Attachment("filename", "pdf", DateTime.Now, string.Empty);
+
             var billAttachedValidAction = new Action(() =>
             {
                 var bill = new Bill(DateTime.Now, samaritan.Id, animal.Id, string.Empty, 1000, false, false, false, null, docPdf, docImage);
@@ -205,27 +206,110 @@ namespace rescute.Tests.DomainTests
             });
 
             billAttachedInvalidAction.Should().Throw<InvalidAttachmentType>();
+        }
 
+        [Fact]
+        public void StatusReportDoesNotAcceptInvalidAttachments()
+        {
+            var samaritan = TestUtility.RandomTestSamaritan();
+            var animal = TestUtility.RandomTestAnimal(samaritan.Id);
 
-            // MedicalDocument
-            var testResultAttachedValidAction = new Action(() =>
+            var docImage = new Attachment("filename", "jpg", DateTime.Now, string.Empty);
+            var docVid = new Attachment("filename", "mp4", DateTime.Now, string.Empty);
+            var docFile = new Attachment("filename", "fle", DateTime.Now, string.Empty);
+            var point = new MapPoint(0, 0);
+
+            var validStatusReport = new StatusReport(DateTime.Now, samaritan.Id, animal.Id, point, string.Empty, docImage, docVid);
+
+            var invalidStatusReport = new Action(() =>
             {
-                var videoAttached = new MedicalDocument(DateTime.Now, samaritan.Id, animal.Id, string.Empty, MedicalDocumentType.DoctorsOrders(), docPdf);
+                var invalidDocImage = new Attachment("filename", "unknown", DateTime.Now, string.Empty);
+                var invalidStatusReport = new StatusReport(DateTime.Now, samaritan.Id, animal.Id, point, string.Empty, invalidDocImage, docVid);
             });
 
-            testResultAttachedValidAction.Should().NotThrow<InvalidAttachmentType>();
-
-            var testResultAttachedInvalidAction = new Action(() =>
-            {
-                var videoAttached = new MedicalDocument(DateTime.Now, samaritan.Id, animal.Id, string.Empty, MedicalDocumentType.IdentityCertificate(), docVid, docFile);
-            });
-
-            testResultAttachedInvalidAction.Should().Throw<InvalidAttachmentType>();
-
-
+            invalidStatusReport.Should().Throw<InvalidAttachmentType>();
         }
 
 
+        [Fact]
+        public void BillOnlyAcceptsValidAttachments()
+        {
+            var samaritan = TestUtility.RandomTestSamaritan();
+            var animal = TestUtility.RandomTestAnimal(samaritan.Id);
+
+            var validAttachmentPdf = new Attachment("filename", "pdf", DateTime.Now, string.Empty);
+            var validAttachmentJpg = new Attachment("filename", "jpg", DateTime.Now, string.Empty);
+            var validAttachmentPng = new Attachment("filename", "png", DateTime.Now, string.Empty);
+
+            var invalidAttachment = new Attachment("filename", "fle", DateTime.Now, string.Empty);
+
+            var validMedicalDocuments = new List<MedicalDocument>();
+            validMedicalDocuments.Add(new MedicalDocument(DateTime.Now, samaritan.Id, animal.Id, "Document description.", MedicalDocumentType.Prescription(), validAttachmentJpg));
+            validMedicalDocuments.Add(new MedicalDocument(DateTime.Now, samaritan.Id, animal.Id, "Document description.", MedicalDocumentType.LabResults(), validAttachmentPng));
+
+            Action action = () =>
+            {
+                var bill = new Bill(DateTime.Now, samaritan.Id, animal.Id, "Bill description.", 1000, false, false, false, validMedicalDocuments, validAttachmentPdf, invalidAttachment);
+            };
+
+            action.Should().Throw<InvalidAttachmentType>();
+        }
+
+        [Fact]
+        public void BillDoesNotAcceptInvalidAttachments()
+        {
+            var samaritan = TestUtility.RandomTestSamaritan();
+            var animal = TestUtility.RandomTestAnimal(samaritan.Id);
+
+            var validAttachmentPdf = new Attachment("filename", "pdf", DateTime.Now, string.Empty);
+            var validAttachmentJpg = new Attachment("filename", "jpg", DateTime.Now, string.Empty);
+            var validAttachmentPng = new Attachment("filename", "png", DateTime.Now, string.Empty);
+
+            var invalidAttachment = new Attachment("filename", "fle", DateTime.Now, string.Empty);
+
+            var validMedicalDocuments = new List<MedicalDocument>();
+            validMedicalDocuments.Add(new MedicalDocument(DateTime.Now, samaritan.Id, animal.Id, "Document description.", MedicalDocumentType.Prescription(), validAttachmentJpg));
+            validMedicalDocuments.Add(new MedicalDocument(DateTime.Now, samaritan.Id, animal.Id, "Document description.", MedicalDocumentType.LabResults(), validAttachmentPng));
+
+            Action action = () =>
+            {
+                var bill = new Bill(DateTime.Now, samaritan.Id, animal.Id, "Bill description.", 1000, false, false, false, validMedicalDocuments, validAttachmentPdf, invalidAttachment);
+            };
+
+            action.Should().Throw<InvalidAttachmentType>();
+        }
+
+        [Fact]
+        public void MedicalDocumentOnlyAcceptsValidAttachments()
+        {
+            var samaritan = TestUtility.RandomTestSamaritan();
+            var animal = TestUtility.RandomTestAnimal(samaritan.Id);
+            var validPdfAttachment = new Attachment("filename", "pdf", DateTime.Now, string.Empty);
+            var validImageAttachment = new Attachment("filename", "jpg", DateTime.Now, string.Empty);
+
+            var medicalDocumentValidAction = new Action(() =>
+            {
+                var medicalDoc = new MedicalDocument(DateTime.Now, samaritan.Id, animal.Id, "Document description", MedicalDocumentType.Prescription(), validPdfAttachment, validImageAttachment);
+            });
+
+            medicalDocumentValidAction.Should().NotThrow<InvalidAttachmentType>();
+        }
+
+        [Fact]
+        public void MedicalDocumentDoesNotAcceptInvalidAttachments()
+        {
+            var samaritan = TestUtility.RandomTestSamaritan();
+            var animal = TestUtility.RandomTestAnimal(samaritan.Id);
+            var validPdfAttachment = new Attachment("filename", "pdf", DateTime.Now, string.Empty);
+            var invalidAttachment = new Attachment("filename", "txt", DateTime.Now, string.Empty);
+
+            var medicalDocumentInvalidAction = new Action(() =>
+            {
+                var medicalDoc = new MedicalDocument(DateTime.Now, samaritan.Id, animal.Id, "Document description", MedicalDocumentType.Prescription(), validPdfAttachment, invalidAttachment);
+            });
+
+            medicalDocumentInvalidAction.Should().Throw<InvalidAttachmentType>();
+        }
 
     }
 }
